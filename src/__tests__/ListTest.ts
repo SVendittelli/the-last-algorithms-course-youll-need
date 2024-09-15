73e434f25054bf23fac41f27d4ae511147971dd2
Sam Vendittelli <sam.vendittelli@hotmail.com>
2024-09-15 12:48:08 +0100

    feat: SinglyLinkedList impl

--- src/__tests__/ListTest.ts
+++ src/__tests__/ListTest.ts
@@ -1,26 +1,50 @@
 export function test_list(list: List<number>): void {
-    list.append(5);
-    list.append(7);
-    list.append(9);
+    const array = [5, 7, 9];
 
-    expect(list.get(2)).toEqual(9);
-    expect(list.removeAt(1)).toEqual(7);
-    expect(list.length).toEqual(2);
+    // Test append and length
+    for (let item of array) {
+        list.append(item);
+    }
+    expect(list.length).toEqual(array.length); // [5,7,9]
 
-    list.append(11);
-    expect(list.removeAt(1)).toEqual(9);
-    expect(list.remove(9)).toEqual(undefined);
-    expect(list.removeAt(0)).toEqual(5);
-    expect(list.removeAt(0)).toEqual(11);
-    expect(list.length).toEqual(0);
+    // Test get
+    array.forEach((item, index) => {
+        expect(list.get(index)).toEqual(item);
+    });
 
-    list.prepend(5);
-    list.prepend(7);
-    list.prepend(9);
+    // Test removeAt
+    expect(list.removeAt(1)).toEqual(array[1]); // [5,9]
+    expect(list.length).toEqual(2); // [5,9]
 
-    expect(list.get(2)).toEqual(5);
-    expect(list.get(0)).toEqual(9);
-    expect(list.remove(9)).toEqual(9);
-    expect(list.length).toEqual(2);
-    expect(list.get(0)).toEqual(7);
+    list.append(11); // [5,9,11]
+    expect(list.removeAt(1)).toEqual(9); // [5,11]
+
+    // Test remove non-existing
+    expect(list.remove(9)).toEqual(undefined); // [5,11]
+    expect(list.removeAt(0)).toEqual(5); // [11]
+    expect(list.removeAt(0)).toEqual(11); // []
+    expect(list.length).toEqual(0); // []
+
+    // Test prepend
+    for (let item of array) {
+        list.prepend(item);
+    }
+    expect(list.length).toEqual(array.length); // [9,7,5]
+
+    array
+        .slice()
+        .reverse()
+        .forEach((item, index) => {
+            expect(list.get(index)).toEqual(item);
+        });
+
+    // Test remove existing
+    expect(list.remove(9)).toEqual(9); // [7,5]
+    expect(list.length).toEqual(2); // [7,5]
+    expect(list.get(0)).toEqual(7); // [7,5]
+
+    list.prepend(1); // [1,7,5]
+    list.insertAt(3, 1); // [1,3,7,5]
+    expect(list.get(1)).toEqual(3);
+    expect(list.length).toEqual(4);
 }
--- src/day1/SinglyLinkedList.ts
+++ src/day1/SinglyLinkedList.ts
@@ -1,12 +1,123 @@
-export default class SinglyLinkedList<T> {
+export default class SinglyLinkedList<T> implements List<T> {
     public length: number;
+    private head: Node<T> | undefined;
 
-    constructor() {}
+    constructor() {
+        this.length = 0;
+    }
 
-    prepend(item: T): void {}
-    insertAt(item: T, idx: number): void {}
-    append(item: T): void {}
-    remove(item: T): T | undefined {}
-    get(idx: number): T | undefined {}
-    removeAt(idx: number): T | undefined {}
+    prepend(item: T): void {
+        const node = new Node(item);
+        node.next = this.head;
+        this.head = node;
+        ++this.length;
+    }
+
+    insertAt(item: T, idx: number): void {
+        if (idx < 0 || idx >= this.length) return;
+        if (idx === 0) return this.prepend(item);
+        if (idx === this.length - 1) return this.append(item);
+
+        const node = new Node(item);
+        let current = this.head;
+        for (let i = 0; i < idx - 1; ++i) {
+            current = current?.next;
+        }
+        if (!current) return;
+        node.next = current.next;
+        current.next = node;
+        ++this.length;
+    }
+
+    append(item: T): void {
+        const node = new Node(item);
+
+        // If the list is empty then set the new item as the head
+        if (!this.head) {
+            this.head = node;
+            ++this.length;
+            return;
+        }
+
+        /**
+         * Get the last node in the list
+         *
+         * @param node The current node
+         * @returns The last node in the list
+         */
+        const getLastNode = (node: Node<T>): Node<T> => {
+            if (!node.next) return node;
+            return getLastNode(node.next);
+        };
+
+        // Otherwise, get the last node and set the new item as the next of the last node
+        const last = getLastNode(this.head);
+        last.next = node;
+        ++this.length;
+    }
+
+    get(index: number): T | undefined {
+        if (index < 0 || index >= this.length) return undefined;
+        let current = this.head;
+        for (let i = 0; i < index; i++) {
+            current = current?.next;
+        }
+        return current?.value;
+    }
+
+    remove(item: T): T | undefined {
+        // If the head is the first item then remove it
+        if (this.head?.value === item) {
+            const returnValue = this.head?.value;
+            this.head = this.head?.next;
+            this.length = Math.max(0, this.length - 1);
+            return returnValue;
+        }
+
+        let current = this.head;
+        while (current?.next) {
+            if (current?.next?.value === item) {
+                const returnValue = current.next.value;
+                current.next = current.next?.next;
+                --this.length;
+                return returnValue;
+            }
+            current = current?.next;
+        }
+        return undefined;
+    }
+
+    removeAt(index: number): T | undefined {
+        if (index < 0 || index >= this.length) return undefined;
+
+        // If the index is 0, remove the head
+        if (index === 0) {
+            const returnValue = this.head?.value;
+            this.head = this.head?.next;
+            this.length = Math.max(0, this.length - 1);
+            return returnValue;
+        }
+
+        // Otherwise, go to the node before the one we want to remove
+        let current = this.head;
+        for (let i = 0; i < index - 1; i++) {
+            current = current?.next;
+        }
+        const returnValue = current?.next?.value;
+        if (!current) {
+            return undefined;
+        }
+        current.next = current?.next?.next;
+        --this.length;
+        return returnValue;
+    }
+}
+
+class Node<T> {
+    public value: T;
+    public next?: Node<T>;
+
+    constructor(value: T) {
+        this.value = value;
+    }
 }
