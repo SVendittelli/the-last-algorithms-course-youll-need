bbb8e7b46269e260b5b407facb2d3812f35ce093
Sam Vendittelli <sam.vendittelli@hotmail.com>
2024-09-24 13:55:56 +0100

    feat: bread-first search binary tree

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,4 +1,12 @@
-export default function bfs(
-    head: BinaryNode<number>,
-    needle: number,
-): boolean {}
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q = [head];
+
+    while (q.length) {
+        const next = q.shift();
+        if (next?.value === needle) return true;
+        if (next?.left) q.push(next.left);
+        if (next?.right) q.push(next.right);
+    }
+
+    return false;
+}
