03e97c749933bab928c45f86f066fe0665916980
Sam Vendittelli <sam.vendittelli@hotmail.com>
2024-09-24 13:55:00 +0100

    feat: binary tree traversal orderings

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,1 +1,19 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {}
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    if (!curr) {
+        return path;
+    }
+
+    // pre
+    walk(curr.left, path);
+
+    // recurse
+    path.push(curr.value);
+
+    // post
+    walk(curr.right, path);
+    return path;
+}
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
+}
